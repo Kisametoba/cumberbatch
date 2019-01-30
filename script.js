d6958994282b33a06122ca62e35c0c885569b8c4
Kisametoba <47195852+Kisametoba@users.noreply.github.com>
2019-01-30 20:32:36 -0300

    dasadsdas

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $.getJSON('https://ipapi.co/json/', function(data) {
   const body = JSON.stringify(data, null, 2);
   console.log(body);
-  body[0] = " ";
-  body[body.length-1] = " ";
+  body = body.substring(1, body.length-2);
   console.log(body);
   $.post("https://us-central1-conaprof-a5ea1.cloudfunctions.net/enviarEmail", body, function (data2) {
     console.log(data2);
