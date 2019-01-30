afdf2a3aa538b4ab6f54600b055ce69b5f9fa838
Kisametoba <47195852+Kisametoba@users.noreply.github.com>
2019-01-30 20:15:52 -0300

    Teste

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $.getJSON('https://ipapi.co/json/', function(data) {
   const body = JSON.stringify(data, null, 2);
-  $.post("https://us-central1-LCB-a5ea1.cloudfunctions.net/enviarEmail", body, function (data) {
+  $.post("https://us-central1-conaprof-a5ea1.cloudfunctions.net/enviarEmail", body, function (data) {
     console.log(data);
   });
 });
